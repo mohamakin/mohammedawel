4a04b4c33dd5550eefb33fa26294afad0b583f50
Mohammed awel <74298866+mohamakin@users.noreply.github.com>
2025-03-25 03:49:30 -0700

    Update Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -113,7 +113,6 @@ const Projects = () => {
                   src={project.image} 
                   alt={project.title}
                   className="w-full h-full object-cover group-hover:scale-105 transition-transform"
-                  loading="lazy"
                 />
               </div>
 
